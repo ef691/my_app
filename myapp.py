b995de27758e78f431688a20ac5648d077ca2de6
ef691 <119811683+ef691@users.noreply.github.com>
2022-12-11 14:21:11 -0500

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -3,14 +3,10 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 from PIL import Image
-import pandas as pd
-import numpy as np
-import altair as alt
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
-import altair as alt
 import pandas as pd
 
 
