6434320e824c9de0ba9dc2dee4557b61599629ab
ef691 <119811683+ef691@users.noreply.github.com>
2022-12-11 14:27:41 -0500

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -126,7 +126,6 @@ st.write("Your age: ",age)
 
 
 ss = pd.DataFrame({
-    "sm_li":np.where(s["web1h"] == 1, 1, 0),
     "income":np.where(s["income"]> 9,np.nan,s["income"]),
     "education":np.where(s["educ2"]> 8,np.nan,s["educ2"]),
     "parent":np.where(s["par"] == 1,1,0),
